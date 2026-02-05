0e040fa04460e7ec8629b6b80a54d78dfba90ab3
BrindhGanesh <mailtobrindhaganesh@gmail.com>
2026-02-05 20:14:31 +0100

    Initial Blazor project commit

---
+++ Models/Appointment.cs
@@ -0,0 +1,10 @@
+namespace HealthcareFrontend.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+        public string PatientName { get; set; } = string.Empty;
+        public string DoctorName { get; set; } = string.Empty;
+        public DateTime AppointmentDate { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+using HealthcareFrontend.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// --- ADD THIS LINE BELOW ---
+// This tells Blazor how to find your API. 
+// Use the exact port (5260) from your API terminal.
+builder.Services.AddScoped(sp => new HttpClient 
+{ 
+    BaseAddress = new Uri("https://apihealth-hgd6fchsh0a3hddf.brazilsouth-01.azurewebsites.net") 
+});
+// ---------------------------
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
